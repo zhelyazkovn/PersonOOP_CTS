c7734334ad4fb8e60b031bfa7db4c59e6f529cd5
zhelyazkovn <zhelyazkovn@gmail.com>
2013-03-12 12:41:26 +0200

    Create PersonDemo.cs

---
+++ PersonDemo.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.PersonDemo
+{
+    class PersonDemo
+    {
+        static void Main(string[] args)
+        {
+            Person testPerson = new Person();
+            Console.WriteLine(testPerson);
+        }
+    }
+}
